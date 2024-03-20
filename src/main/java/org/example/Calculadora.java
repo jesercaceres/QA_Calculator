f16a21536682ecb91252d1b7372b8c06cfa78ad9
Jeser Caceres <jeser.caceres01@yahoo.com>
2024-03-19 22:10:19 -0300

    Calculadora Q&A

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculadora {
+    public int somar(int num1, int num2) {
+
+        return num1 + num2;
+    }
+
+    public int subtrair(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public double dividir(double num1, double num2) {
+
+        return num1 / num2;
+    }
+}
