a871650c3150b5d50fe7f966b4c712765b128f37
Jeser Caceres <jeser.caceres01@yahoo.com>
2024-03-24 12:05:27 -0300

    Implementação de comentários usando padrão JAVADOC

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,23 +1,53 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Esta classe representa uma calculadora simples que realiza operações básicas como soma,
+ * subtração, multiplicação e divisão.
+ *
+ * @author Jéser Cáceres Marcelino
+ */
 public class Calculadora {
+    /**
+     * Realiza a adição de dois números inteiros.
+     *
+     * @param num1 O primeiro número a ser somado.
+     * @param num2 O segundo número a ser somado.
+     * @return O resultado da adição.
+     */
     public int somar(int num1, int num2) {
-
         return num1 + num2;
     }
 
+    /**
+     * Realiza a subtração de dois números inteiros.
+     *
+     * @param num1 O número do qual será subtraído.
+     * @param num2 O número a ser subtraído.
+     * @return O resultado da subtração.
+     */
     public int subtrair(int num1, int num2) {
         return num1 - num2;
     }
 
+    /**
+     * Realiza a multiplicação de dois números reais.
+     *
+     * @param num1 O primeiro número a ser multiplicado.
+     * @param num2 O segundo número a ser multiplicado.
+     * @return O resultado da multiplicação.
+     */
     public double multiplicar(double num1, double num2) {
         return num1 * num2;
     }
 
+    /**
+     * Realiza a divisão de dois números reais.
+     *
+     * @param num1 O número que será dividido.
+     * @param num2 O divisor.
+     * @return O resultado da divisão.
+     */
     public double dividir(double num1, double num2) {
-
         return num1 / num2;
     }
 }
--- src/test/java/CalculadoraJunitTest.java
+++ src/test/java/CalculadoraJunitTest.java
@@ -2,9 +2,20 @@ import org.example.Calculadora;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Classe de teste para a classe Calculadora, utilizando JUnit 5.
+ * Testa os métodos da classe Calculadora em diferentes cenários.
+ * Esta classe utiliza anotações do JUnit 5 para os testes.
+ *
+ * @author Jéser Cáceres Marcelino
+ */
 public class CalculadoraJunitTest {
     Calculadora calc = new Calculadora();
 
+    /**
+     * Testa o método somar da classe Calculadora.
+     * Verifica a soma de dois números inteiros em diferentes cenários.
+     */
     @Test
     public void somarDoisNumeros() {
         int soma = calc.somar(10, 20);
@@ -14,16 +25,19 @@ public class CalculadoraJunitTest {
         soma = calc.somar(3, 0);
         Assertions.assertEquals(3, soma);
 
-        //CENÁRIO DE TESTE 3: SOMA DE DOIS VALORES SENDO AMBOS SÃO ZERO
+        //CENÁRIO DE TESTE 3: SOMA DE DOIS VALORES SENDO AMBOS ZERO
         soma = calc.somar(0, 0);
         Assertions.assertEquals(0, soma);
-
 
         //CENÁRIO DE TESTE 4: SOMA DE DOIS VALORES SENDO UM NEGATIVO
         soma = calc.somar(3, -1);
         Assertions.assertEquals(2, soma);
     }
 
+    /**
+     * Testa o método subtrair da classe Calculadora.
+     * Verifica a subtração de dois números inteiros em diferentes cenários.
+     */
     @Test
     public void subtrairDoisNumeros() {
         int subtrair = calc.subtrair(10, 2);
@@ -37,7 +51,7 @@ public class CalculadoraJunitTest {
         subtrair = calc.subtrair(3, 0);
         Assertions.assertEquals(3, subtrair);
 
-        //CENÁRIO DE TESTE 3: SUBTRAÇÃO DE DOIS VALORES SENDO AMBOS SÃO ZERO
+        //CENÁRIO DE TESTE 3: SUBTRAÇÃO DE DOIS VALORES SENDO AMBOS ZERO
         subtrair = calc.subtrair(0, 0);
         Assertions.assertEquals(-0, subtrair);
 
@@ -46,42 +60,49 @@ public class CalculadoraJunitTest {
         Assertions.assertEquals(4, subtrair);
     }
 
+    /**
+     * Testa o método multiplicar da classe Calculadora.
+     * Verifica a multiplicação de dois números inteiros em diferentes cenários.
+     */
     @Test
     public void multiplicarDoisNumeros() {
-        //CENÁRIO DE TESTE 1 MULTIPLICAÇÃO DE DOIS VALORES
+        //CENÁRIO DE TESTE 1: MULTIPLICAÇÃO DE DOIS VALORES
         double multiplicacao = calc.multiplicar(3, 7);
         Assertions.assertEquals(21, multiplicacao);
 
-        //CENÁRIO DE TESTE 2 MULTIPLICAÇÃO DE DOIS VALORES SENDO UM ZERO
+        //CENÁRIO DE TESTE 2: MULTIPLICAÇÃO DE DOIS VALORES SENDO UM ZERO
         multiplicacao = calc.multiplicar(3, 0);
         Assertions.assertEquals(0, multiplicacao);
 
-        //CENÁRIO DE TESTE 3 MULTIPLICAÇÃO, AMBOS SÃO ZERO
+        //CENÁRIO DE TESTE 3: MULTIPLICAÇÃO, AMBOS VALORES SENDO ZERO
         multiplicacao = calc.multiplicar(0, 0);
         Assertions.assertEquals(0, multiplicacao);
 
-        //CENÁRIO DE TESTE 4 MULTIPLICAÇÃO DE DOIS VALORES SENDO UM NEGATIVO
+        //CENÁRIO DE TESTE 4: MULTIPLICAÇÃO DE DOIS VALORES SENDO UM NEGATIVO
         multiplicacao = calc.multiplicar(3, -1);
         Assertions.assertEquals(-3, multiplicacao);
     }
 
+    /**
+     * Testa o método dividir da classe Calculadora.
+     * Verifica a divisão de dois números em diferentes cenários.
+     */
     @Test
     public void dividirDoisNumeros() {
-        //CENÁRIO DE TESTE 1 DIVISÃO DE DOIS VALORES
+        //CENÁRIO DE TESTE 1: DIVISÃO DE DOIS VALORES
         double divisao = calc.dividir(4, 2);
         Assertions.assertEquals(2, divisao);
 
-        //CENÁRIO DE TESTE 2: DIVISÃO DE DOIS VALORES SENDO UM ZERO
+        //CENÁRIO DE TESTE 2: DIVISÃO POR ZERO
         divisao = calc.dividir(3, 0);
         Assertions.assertEquals(Double.POSITIVE_INFINITY, divisao);
 
-        //CENÁRIO DE TESTE 3 DIVISÃO, AMBOS SÃO ZERO
+        //CENÁRIO DE TESTE 3: DIVISÃO, AMBOS VALORES SENDO ZERO
         divisao = calc.dividir(0, 0);
         Assertions.assertTrue(Double.isNaN(divisao));
 
-        //CENÁRIO DE TESTE 4 DIVISÃO DE DOIS VALORES SENDO UM NEGATIVO
+        //CENÁRIO DE TESTE 4: DIVISÃO DE DOIS VALORES SENDO UM NEGATIVO
         divisao = calc.dividir(3, -1);
         Assertions.assertEquals(-3, divisao);
     }
-
 }
